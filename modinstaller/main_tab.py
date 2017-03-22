6009a73552d83153233ece59059ae487e3dfb739
petitsurume <gamemannew2013@yahoo.co.jp>
2017-03-22 17:51:58 +0900

    WIP

--- modinstaller/main_tab.py
+++ modinstaller/main_tab.py
@@ -1,8 +1,8 @@
 import wx
 import modinstaller.minecraft_profile
 import modinstaller.main_window
+import modinstaller.tabs.meta
 
 notebook = wx.Notebook(modinstaller.main_window.window)
-
-
+notebook.AddPage(modinstaller.tabs.meta.MetaPage(notebook), "情報")
 modinstaller.main_window.add(notebook)
---
+++ modinstaller/tabs/meta.py
@@ -0,0 +1,6 @@
+import wx
+import modinstaller.main_tab
+
+class MetaPage(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self,parent)
